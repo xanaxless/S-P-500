25939ea0526109c3af2fa37e547dfc5be20e691a
Yerkebulan Serikov <serikov.2003.18@gmail.com>
2022-05-07 15:51:19 +0600

    Initial Commit

---
+++ S&P 500/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S&P 500
+//
+//  Created by Yerkebulan Serikov on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
